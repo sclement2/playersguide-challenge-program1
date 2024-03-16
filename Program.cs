a74a66c5f320ef31ad53a0e290dea3fea81946cc
sclement2 <sclement2@gmail.com>
2024-03-16 23:55:25 +0000

    Refactor Consolas game based on ChatGPT feedback. More modular

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ namespace dotnetcore
 {
     class Program
     {
+        enum RoundResult { Short, Long, Hit }
+
         static void Main(string[] args)
         {
             PlayGame();
@@ -11,78 +13,48 @@ namespace dotnetcore
 
         static void PlayGame()
         {
+            // Game variables
+            const int minRange = 0; // min range for the location of the Manticore
+            const int maxRange = 100; // max range for the location of the Manticore
+            int currentRound = 1;
+            const int cityStartHealth = 15; // starting health for the city
+            const int manticoreStartHealth = 10; // starting health for the Manticore
+
+            int manticoreDamage = 2; // amount of damage the Manticore does to the city each round
+
             bool playAgain = true;
 
             while (playAgain) // Repeat game play loop
             {
-                // Game variables
-                const int minRange = 0; // min range for the location of the Manticore
-                const int maxRange = 100; // max range for the location of the Manticore
-                int currentRound = 1;
-                const int cityHealth = 15; // starting health for the city
-                int currentCityHealth = cityHealth; // current health level
-                const int manticoreHealth = 10; // starting health for the Manticore
-                int currentManticoreHealth = manticoreHealth; // current Manticor health level
-                int manticoreDamage = 2; // amount of damage the Manticore does to the city each round
-                int cannonDamage; // cannon damage which is a calculated value per round
-                string cityWinsText = "\nThe Manticore has been destroyed! The city of Consolas has been saved!";
-                string manticoreWinsText = "\nOh No! Consolas has been destroyed! The world is doomed.";
-                string mutualDestructionText = "\nOh No! The Manticore and Consolas have destroyed each other. What will happen to us?";
+                int cityCurrentHealth = cityStartHealth; // current health level
+                int manticoreCurrentHealth = manticoreStartHealth; // current Manticor health level
+                int maxRound = 12; // Highest round the game should play before exiting if no one has won
 
-                const string separator = "-------------------------------------------------";
-
-                bool endGame = false; // holds status of whether the game should end or not
-                bool cityDestroyed = false;
-                bool manticoreDestroyed = false;
-
-                int player1Input = GetUserNumber(minRange, maxRange);
+                int player1Input = GetRange(minRange, maxRange);
                 if (player1Input == -1) // Error occurred
+                {
+                    Console.WriteLine("Next time please follow the instructions. Program is exiting.");
                     Environment.Exit(0);
+                }
 
                 Console.WriteLine($"Player 1 stationed the Manticore at {player1Input}.\n");
                 //Console.Clear();
 
                 Console.WriteLine("Player 2, it is your turn.");
                 Console.WriteLine("You need to guess the Manticore's range.");
-                while (!endGame) // Game play loop
+                while (manticoreCurrentHealth > 0 && cityCurrentHealth > 0 && currentRound <= maxRound) // Game play loop
                 {
+                    int cannonDamage = MagicCannon(currentRound);
+                    GameDisplay(currentRound, cityCurrentHealth, cityStartHealth, manticoreCurrentHealth, manticoreStartHealth, cannonDamage);
 
-                    cannonDamage = MagicCannon(currentRound);
-                    Console.WriteLine(separator);
-                    Console.WriteLine($"STATUS: Round: {currentRound}  City: {currentCityHealth}/{cityHealth}  Manticore: {currentManticoreHealth}/{manticoreHealth}");
-                    Console.WriteLine($"The cannon is expected to deal {cannonDamage} damage this round.");
-
-                    if (GuessNumber(player1Input))
-                    {
-                        currentManticoreHealth = CalculateHealth(currentManticoreHealth, cannonDamage);
-                        manticoreDestroyed = CheckHealth(currentManticoreHealth);
-                    }
-                    currentCityHealth = CalculateHealth(currentCityHealth, manticoreDamage);
-                    cityDestroyed = CheckHealth(currentCityHealth);
-
-                    // check if anyone is dead
-                    if (cityDestroyed && manticoreDestroyed)
-                    {
-                        Console.WriteLine(mutualDestructionText);
-                        endGame = true;
-                        continue;
-                    }
-                    else if (manticoreDestroyed)
-                    {
-                        Console.WriteLine(cityWinsText);
-                        endGame = true;
-                        continue;
-                    }
-                    else if (cityDestroyed)
-                    {
-                        Console.WriteLine(manticoreWinsText);
-                        endGame = true;
-                        continue;
-                    }
+                    RoundResult result = GetRoundResult(player1Input);
+                    if (result == RoundResult.Hit)
+                        manticoreCurrentHealth -= cannonDamage; // Manticore is damaged only if player 2 guesses the correct target range for the Manticore.
+                    cityCurrentHealth -= manticoreDamage; // Manticore always does damage each round as long as it is alive
                     currentRound++;
-
-                    if (currentRound == 12) endGame = true;
                 }
+
+                EndGame(cityCurrentHealth, manticoreCurrentHealth);
 
                 // Play again?
                 Console.ForegroundColor = ConsoleColor.White;
@@ -95,61 +67,79 @@ namespace dotnetcore
         }
 
         /// <summary>
+        /// Determine what the outcome of the game is since an end condition has been reached
+        /// </summary>
+        /// <param name="cityCurrentHealth"></param>
+        /// <param name="manticoreCurrentHealth"></param>
+        static void EndGame(int cityCurrentHealth, int manticoreCurrentHealth)
+        {
+            if (cityCurrentHealth <= 0 && manticoreCurrentHealth <= 0)
+                Console.WriteLine("\nOh No! The Manticore and Consolas have destroyed each other. What will happen to us?");
+            else if (manticoreCurrentHealth <= 0)
+                Console.WriteLine("\nThe Manticore has been destroyed! The city of Consolas has been saved!");
+            else if (cityCurrentHealth <= 0)
+                Console.WriteLine("\nOh No! Consolas has been destroyed! The world is doomed.");
+            else
+                Console.WriteLine("\nThe game ended in a stalemate. Both sides need to regroup, repair and prepare for battle again.");
+        }
+
+        static void GameDisplay(int currentRound, int cityCurrentHealth, int cityStartHealth, int manticoreCurrentHealth, int manticoreStartHealth, int cannonDamage)
+        {
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine($"STATUS: Round: {currentRound}  City: {cityCurrentHealth}/{cityStartHealth}  Manticore: {manticoreCurrentHealth}/{manticoreStartHealth}");
+            Console.WriteLine($"The cannon is expected to deal {cannonDamage} damage this round.");
+        }
+
+        /// <summary>
+        /// Determine if player2 guessed the correct range for the Manticore.
+        /// </summary>
+        /// <param name="targetRange"></param>
+        /// <returns>(RoundResult) enum for the possible results of the cannon shot</returns>
+        static RoundResult GetRoundResult(int targetRange)
+        {
+            string prompt = "Enter desired cannon range";
+            string tooShort = "That round FELL SHORT of the target.";
+            string tooLong = "That round OVERSHOT the target.";
+            string directHit = "That round was a DIRECT HIT!";
+
+            int player2Guess = AskForNumber(prompt);
+            if (player2Guess == -1)
+            {
+                Console.WriteLine("Next time please follow the instructions. Program is exiting.");
+                Environment.Exit(0);
+            }
+
+            if (player2Guess == targetRange)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{directHit}");
+                Console.ForegroundColor = ConsoleColor.White;
+                return RoundResult.Hit;
+            }
+            else if (player2Guess < targetRange)
+            {
+                Console.WriteLine($"{tooShort}");
+                return RoundResult.Short;
+            }
+            else
+            {
+                Console.WriteLine($"{tooLong}");
+                return RoundResult.Long;
+            }
+        }
+
+        /// <summary>
         /// Routine for player 1 to supply the range of the Manticore
         /// </summary>
         /// <param name="minNumber"></param>
         /// <param name="maxNumber"></param>
         /// <returns>(int) Distance to the Manticore</returns>
-        static int GetUserNumber(int minNumber, int maxNumber)
+        static int GetRange(int minNumber, int maxNumber)
         {
             string prompt = "Player 1, how far away from the city do you want to station the Manticore?";
             int userNumber = AskForNumberInRange(prompt, minNumber, maxNumber);
 
-            if (userNumber == -1)
-            {
-                Console.WriteLine("Next time please follow the instructions. Program is exiting.");
-                Environment.Exit(0); // Error condition
-            }
             return userNumber;
-        }
-
-        /// <summary>
-        /// Guessing method for determining if player2 guessed the correct range for the Manticore
-        /// </summary>
-        /// <param name="player1Input"></param>
-        /// <returns>(bool) Correct guess was made for the Manticore's range</returns>
-        static bool GuessNumber(int player1Input)
-        {
-            string toShort = "That round FELL SHORT of the target.";
-            string toLong = "That round OVERSHOT the target.";
-            string directHit = "That round was a DIRECT HIT!";
-            string prompt = "Enter desired cannon range";
-
-            int player2Guess = AskForNumber(prompt);
-
-            if (player2Guess == -1)
-            {
-                Console.WriteLine("Next time please follow the instructions. Program is exiting.");
-                Environment.Exit(0); // Error condition
-            }
-
-            if (player2Guess < player1Input)
-            {
-                Console.WriteLine($"{toShort}");
-                return false;
-            }
-            else if (player2Guess > player1Input)
-            {
-                Console.WriteLine($"{toLong}");
-                return false;
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"{directHit}");
-                Console.ForegroundColor = ConsoleColor.White;
-                return true; // guessed correctly
-            }
         }
 
         /// <summary>
@@ -164,27 +154,10 @@ namespace dotnetcore
 
             if (round % DivideByThree == 0 && round % DivideByFive == 0)
                 return 10;
-            else if (round % DivideByThree == 0)
-                return 3;
-            else if (round % DivideByFive == 0)
+            else if (round % DivideByThree == 0 || round % DivideByFive == 0)
                 return 3;
             else
                 return 1;
-        }
-
-        // update health value based on supplied damage
-        static int CalculateHealth(int currentHealth, int damage)
-        {
-            currentHealth -= damage;
-            if (currentHealth <= 0) currentHealth = 0;
-            return currentHealth;
-        }
-
-        // figure out if alive or not
-        static bool CheckHealth(int currentHealth)
-        {
-            if (currentHealth == 0) return true;
-            return false;
         }
 
         /// <summary>
